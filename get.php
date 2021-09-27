036ca55e263f97d2f9ec1fe3f94eecb02532c32c
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-09-28 00:00:56 +0800

    check box shows up in array

--- get.php
+++ get.php
@@ -1,8 +1,35 @@
 <?php
 
 
+
 var_dump($_GET);
 
 
-?>
+if(isset($_GET['socialmedia']))
+    {
+        if(in_array('facebook', $_GET['socialmedia']))
+        {
+            echo "facebook was checked!";
+        }};
+    
+    
+if(isset($_GET['socialmedia']))
+{
+        if(in_array('twitter', $_GET['socialmedia'])){
+            echo "twitter was checked!";
+        }};
+    
+    
+if(isset($_GET['socialmedia'])) {
 
+
+        if(in_array('tiktok', $_GET['socialmedia'])){
+            echo "tiktok was checked!";
+        }};
+if(isset($_GET['socialmedia']))  
+{
+        if(in_array('instagram', $_GET['socialmedia'])){
+            echo "instagram was checked!";
+      }};
+
+?>
